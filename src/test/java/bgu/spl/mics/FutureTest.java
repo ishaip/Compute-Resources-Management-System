b4bc1a51375e954daae3735104292448c3053074
spl211 <ishai.picus@gmail.com>
2021-12-04 11:21:13 +0200

    Initial commit

---
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -0,0 +1,25 @@
+package bgu.spl.mics;
+
+import junit.framework.TestCase;
+
+public class FutureTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGet() {
+    }
+
+    public void testResolve() {
+    }
+
+    public void testIsDone() {
+    }
+
+    public void testTestGet() {
+    }
+}
