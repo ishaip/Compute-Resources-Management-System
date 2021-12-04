21f0209c2d2292c92e45f27349b7dad19b0d93ac
itaygenkin <itay.genkin@gmail.com>
2021-12-04 16:58:13 +0200

    init

--- src/test/java/bgu/spl/mics/FutureTest.java
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -4,8 +4,10 @@ import junit.framework.TestCase;
 
 public class FutureTest extends TestCase {
 
+    private Future<String> future;
+
     public void setUp() throws Exception {
-        super.setUp();
+//        super.setUp();
     }
 
     public void tearDown() throws Exception {
