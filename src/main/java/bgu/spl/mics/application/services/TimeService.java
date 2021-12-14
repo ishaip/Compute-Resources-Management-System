d00ad2b8ad3672ef9dcf2554a4105cbf96d81bd8
picus@wincs.cs.bgu.ac.il <picus>
2021-12-14 15:49:42 +0200

    -messageBus complete, time service as a new object, needs to be changed to a single time-

--- src/main/java/bgu/spl/mics/application/services/TimeService.java
+++ src/main/java/bgu/spl/mics/application/services/TimeService.java
@@ -1,6 +1,11 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.MessageBusImpl;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.broadcast.TerminateBroadcast;
+import bgu.spl.mics.application.broadcast.TickBroadcast;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * TimeService is the global system timer There is only one instance of this micro-service.
@@ -11,17 +16,32 @@ import bgu.spl.mics.MicroService;
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
-public class TimeService extends MicroService{
 
-	public TimeService() {
-		super("Change_This_Name");
-		// TODO Implement this
+public class TimeService extends MicroService{
+	private int time;
+	private int speed;
+	private int duration;
+
+
+	public TimeService(String name, int speed, int duration) {
+		super(name);
+		time = 0;
+		this.speed=speed;
+		this.duration=duration;
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
-		
+		while(time < duration) {
+			time = time + 1;
+			MessageBusImpl.getInstance().sendBroadcast(new TickBroadcast(time));
+			try {
+				wait(speed);
+			} catch (InterruptedException e) {
+				//do nothing
+			}
+		}
+		MessageBusImpl.getInstance().sendBroadcast(new TerminateBroadcast());
 	}
 
 }
