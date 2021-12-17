37463db59aae9d056939e974b8925ef2248e4d9a
itaygenkin <itay.genkin@gmail.com>
2021-12-17 13:27:39 +0200

    17/12 - adding toString() method to ConferenceInformation and adding new field - "publications" (ArrayList)

--- src/main/java/bgu/spl/mics/application/objects/ConfrenceInformation.java
+++ src/main/java/bgu/spl/mics/application/objects/ConfrenceInformation.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.ArrayList;
+
 /**
  * Passive object representing information on a conference.
  * Add fields and methods to this class as you see fit (including public methods and constructors).
@@ -8,6 +10,7 @@ public class ConfrenceInformation {
 
     private String name;
     private int date;
+    private ArrayList<Model> publications = new ArrayList<>();
 
     public ConfrenceInformation(String name, int date){
         this.name = name;
@@ -21,4 +24,35 @@ public class ConfrenceInformation {
     public String getName(){
         return name;
     }
+
+    public void addPublication (Model model){
+        publications.add(model);
+    }
+
+    public ArrayList<Model> getPublications (){
+        return publications;
+    }
+
+    public String toString() {
+        String str = "";
+
+        str += "\"name\": \"" + name + "\",\n";
+        str += "\"date\": " + Integer.toString(date) + ",\n";
+        str += "\"publications\": [\n\t";
+        for (int i = 0; i < publications.size(); i++){
+            str += "{\n\t\t" + publications.get(i).toString() + "\n}";
+            if ( i < publications.size() - 1 )
+                str += ",";
+            str += "\n";
+        }
+        return str;
+    }
 }
+
+
+
+
+
+
+
+
